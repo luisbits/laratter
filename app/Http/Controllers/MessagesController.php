38e99d6ca79e7a44c2aa0aea7d602e80a97096de
LuisBits <soyluisfrias@gmail.com>
2018-03-18 08:56:14 -0500

    Clase 11 - Traer un solo resultado de la base de datos

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Message;
+
+class MessagesController extends Controller
+{
+    public function show(Message $message)
+    {
+        //$message = Message::find($id);
+
+        return view('message.show',[
+            'message' => $message
+        ]);
+    }
+}
