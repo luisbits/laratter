3127a327f343a81b0cf96f4442381f966e1e1629
LuisBits <soyluisfrias@gmail.com>
2018-03-18 13:04:10 -0500

    Ajustando columnas en front de listado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,25 @@
     </nav>
 </div>
 <div class="row">
-    <div class="col-12">
+    <div class="col-9">
+    @forelse($messages as $message)
+    <div class="card bg-light mb-3">
+        <div class="card-header">{{ $message->id }}</div>
+        <div class="card-body">
+            <img src="{{ $message->image }}" alt="" class="img-thumbnail">
+            <p class="card-text">
+                {{ $message->content }}
+            </p>
+            <a href="/messages/{{ $message->id }}">Leer más</a>
+        </div>
+    </div>
+    @empty
+    <p class="text-center">
+        No hay mensajes
+    </p>
+    @endforelse
+    </div>
+    <div class="col-3">
         <form action="messages/create" method="POST">
             <div class="form-group">
                 <input type="text" name="message" class="form-control @if($errors->has('message')){{ 'is-invalid' }} @endif)" placeholder="¿Qué estás pensando?">
@@ -26,18 +44,5 @@
             <button class="btn btn-primary pull-right" type="submit">Guardar</button>
         </form>
     </div>
-    @forelse($messages as $message)
-    <div class="col-6">
-        <img src="{{ $message->image }}" alt="" class="img-thumbnail">
-        <p class="card-text">
-            {{ $message->content }}
-            <a href="/messages/{{ $message->id }}">Leer más</a>
-        </p>
-    </div>
-    @empty
-    <p class="text-center">
-        No hay mensajes
-    </p>
-    @endforelse
 </div>
 @endsection
