ee8a230ea00558198423d22f00f4b3aca4dcaa68
LuisBits <soyluisfrias@gmail.com>
2018-03-18 12:55:28 -0500

    MessageFactory agregado

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Message::class, function (Faker $faker) {
+    return [
+        'content' => $faker->realText(random_int(20,160)),
+        'image' => $faker->imageUrl()
+    ];
+});
